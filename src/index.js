4dfe949088ca520ad766380d2b2b37e66267dca0
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-15 19:13:25 +0300

    Render head and footer, add images folder.

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+import logoSVG from "./images/checklist.svg";
+
+const body = document.querySelector("body");
+
+const renderBar = (function () {
+  const header = (name) => {
+    const head = document.createElement("header");
+    const logo = document.createElement("img");
+    logo.src = logoSVG;
+    logo.alt = "logo";
+    const logoName = document.createElement("h1");
+    logoName.textContent = name;
+    head.appendChild(logo);
+    head.appendChild(logoName);
+    body.prepend(head);
+  };
+  const footer = (text) => {
+    const foot = document.createElement("footer");
+    foot.textContent = `${text} ${new Date().getFullYear()}`;
+    body.append(foot);
+  };
+
+  return { header, footer };
+})();
+
+renderBar.header("Boy Next Door");
+renderBar.footer("©");
