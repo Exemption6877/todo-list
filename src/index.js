34036c53fe7e7813d11dd53cadaaef8a55211b43
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-02-15 21:02:31 +0300

    Add dialog functionality

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import logoSVG from "./images/checklist.svg";
 
 const body = document.querySelector("body");
 
+const showButton = document.querySelector("#create");
+const closeButton = document.querySelector("#close-dialog");
+const dialog = document.querySelector("dialog");
+
+showButton.addEventListener("click", (event) => {
+  dialog.show();
+  event.preventDefault();
+});
+
+closeButton.addEventListener("click", (event) => {
+  dialog.close();
+  event.preventDefault();
+});
+
 const renderBar = (function () {
   const header = (name) => {
     const head = document.createElement("header");
@@ -24,5 +38,36 @@ const renderBar = (function () {
   return { header, footer };
 })();
 
+// const renderEntryElement = (function () {
+//     const
+// })();
+
+// I will pass inside a class objects to generate an entry.
+// TODO: static id generator in entry class.
+const renderContent = (function () {
+  const menu = document.querySelector(".menu");
+  // will need to pass some kind of an array,
+  // will be using placeholder for now.
+
+  const entries = () => {
+    const content = document.querySelector(".content");
+    const entry = document.createElement("div");
+    entry.classList.add("entry");
+
+    // from array => entry.text
+    const title = document.createElement("h3");
+    title.textContent = "Placeholder Title";
+
+    const description = document.createElement("p");
+    description.textContent = "Description Test";
+
+    entry.append(title, description);
+    content.append(entry);
+  };
+
+  return { entries };
+})();
+
+renderContent.entries();
 renderBar.header("Boy Next Door");
 renderBar.footer("©");
